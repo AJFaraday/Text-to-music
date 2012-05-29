21a58dba1dcef3d63ceb1af6ea74ff04f895f428
A Faraday <andrewfaraday@hotmail.co.uk>
2012-05-29 18:40:59 +0100

    Small change to connection message

--- lib/pd-connect.rb
+++ lib/pd-connect.rb
@@ -4,15 +4,18 @@ require 'lib/character'
 class PureData
 
   attr_accessor :connection
+  attr_accessor :hostname
+  attr_accessor :port
 
   # this should set up a port into pd
   def initialize
     hostname = '127.0.0.1'
     port = '3939'
     begin
+      puts "Connection established on #{hostname}:#{port}"
       self.connection = TCPSocket.open hostname, port
     rescue Errno::ECONNREFUSED
-      puts "Connection Refused! Please ensure ruby_interact.pd is running in puredata and listening on #{hostname}:#{port}"
+      puts "Connection refused! Please ensure ruby_interact.pd is running in puredata and listening on #{hostname}:#{port}"
     end
   end
 
