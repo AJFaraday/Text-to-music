2aa5789f564fedb1907ce2087407f6558bbca8ae
A Faraday <andrewfaraday@hotmail.co.uk>
2012-05-30 23:47:45 +0100

    Tweaked PureData send location script (send reset message if location isn't present) Added initial landing sub-patch for location information TODO Average border points when location is present, but not geo, for a rough center of the location.

--- lib/pd-connect.rb
+++ lib/pd-connect.rb
@@ -39,8 +39,11 @@ class PureData
   # accepts the 'place' and 'geo' hashes from a tweet and dumps parts of it to puredata
   # command will be along the lines of "location GB Guildford 51.24008618 -0.57108614"
   def send_location(place, geo)
-    if place and geo
-      connection.puts("location #{place[:country_code]} #{place[:name]} #{geo[:coordinates][0]} #{geo[:coordinates][1]};")
+    if place and !place.nil?
+      if geo and !geo.nil?
+        # TODO add an extra case to average the boundary co-ordinates from place in the absence of geo
+        connection.puts("location #{place[:country_code]} #{place[:name]} #{geo[:coordinates][0]} #{geo[:coordinates][1]};")
+      end
     else
       connection.puts("location reset;")
     end
